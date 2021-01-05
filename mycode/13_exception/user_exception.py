66a345845db4d4696615c87926c220bd457c586a
youminji62 <youminji62@gmail.com>
2021-01-05 11:45:41 +0900

    add exception

---
+++ mycode/13_exception/user_exception.py
@@ -0,0 +1,16 @@
+# 사용자 정의 예외 클래스 선언
+class NegativePriceException(Exception):
+    # constructor 선언
+    def __init__(self):
+        print('Price can\'t be Negative')
+        raise AttributeError
+
+def calc_price(value):
+    price = value * 100
+    if price < 0:
+        # NegativePriceException 강제로 발생한다
+        raise NegativePriceException
+    return price
+
+print(calc_price(10))
+print(calc_price(-10))
